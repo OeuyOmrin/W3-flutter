0e2d9d7509ed2acb6594f6e9fab0a0938cc3e8df
OeuyOmrin <omrin.oeuy@student.cadt.edu.kh>
2026-05-29 08:34:55 +0700

    Update main3.dart

--- main3.dart
+++ main3.dart
@@ -2,57 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyApp(),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body:Padding(padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 100,
-              decoration: const ShapeDecoration(
-                gradient: LinearGradient(
-                  colors: [Color(0xFFADD8E6), Color(0xFF00008B)],
-                  begin: Alignment.centerLeft, 
-                  end: Alignment.centerRight, 
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 100,
+                decoration: const ShapeDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFFADD8E6), Color(0xFF00008B)],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                  shape: StadiumBorder(),
+                ),
+                child: const Center(
+                  child: Text(
+                    'OOP',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
-                    shape: StadiumBorder(), ),
-              child: const Center(
-                child: Text(
-                  'OOP',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            Container(
-              height: 100,
-              decoration: const ShapeDecoration(
-                gradient: LinearGradient(
-                  colors: [Color(0xFFADD8E6), Color(0xFF00008B)],
-                  begin: Alignment.centerLeft, 
-                  end: Alignment.centerRight,
+              Container(
+                height: 100,
+                decoration: const ShapeDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFFADD8E6), Color(0xFF00008B)],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
                   ),
-                shape: StadiumBorder()),
-
+                  shape: StadiumBorder(),
+                ),
                 child: const Center(
                   child: Text(
                     'DART',
@@ -63,38 +55,37 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                 ),
+              ),
 
-            ), 
+              const SizedBox(height: 20),
 
-            const SizedBox(height: 20),
-
-            Container(
-              height: 100,
-              
-              decoration: const ShapeDecoration(
-                gradient: LinearGradient(
-                  colors: [Color(0xFFADD8E6), Color(0xFF00008B)],
-                  begin: Alignment.centerLeft, 
-                  end: Alignment.centerRight ),
-                shape: StadiumBorder()),
+              Container(
+                height: 100,
+                decoration: const ShapeDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFFADD8E6), Color(0xFF00008B)],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                  shape: StadiumBorder(),
+                ),
                 child: const Center(
-                child: Text(
-                  'FLUTTER',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                  child: Text(
+                    'FLUTTER',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-                
-            ),
 
-            const SizedBox(height: 20),
-          ],
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
-    );
-  }
-
+    ),
+  );
 }
