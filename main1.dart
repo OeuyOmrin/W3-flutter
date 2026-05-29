f64f09841df9cdcc3c77315fb1cf9d9c8c1e9335
OeuyOmrin <omrin.oeuy@student.cadt.edu.kh>
2026-05-29 08:33:27 +0700

    Update main1.dart

--- main1.dart
+++ main1.dart
@@ -2,24 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: MyApp(),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Text(
-          "Hello My Name is Nirmo",
-          style: TextStyle(fontSize: 30),
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "Hello My Name is Nirmo",
+            style: TextStyle(fontSize: 30),
+          ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
