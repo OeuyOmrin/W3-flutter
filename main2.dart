accf9081b3ca31dee925ffd7dc1d57fdeb13d0f1
OeuyOmrin <omrin.oeuy@student.cadt.edu.kh>
2026-05-29 08:34:33 +0700

    Refactor MyContainer to use Scaffold directly

--- main2.dart
+++ main2.dart
@@ -1,42 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: MyContainer( ),
-  ));
-}
-
-class MyContainer extends StatelessWidget {
-  const MyContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(40), 
-          margin: const EdgeInsets.all(50), 
-          color: Colors.blue[300],
-
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
           child: Container(
-            decoration: BoxDecoration(
-              color: Colors.blue[600], 
-              borderRadius: BorderRadius.circular(20),
-            ), 
+            padding: const EdgeInsets.all(40),
+            margin: const EdgeInsets.all(50),
+            color: Colors.blue[300],
 
-            child: const Center(
-              child: Text(
-                "CADT STUDENTS", 
-                style: TextStyle(
-                  color: Colors.white, 
-                  fontSize: 24, 
-                  fontWeight: FontWeight.bold, 
-                )
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue[600],
+                borderRadius: BorderRadius.circular(20),
+              ),
+
+              child: const Center(
+                child: Text(
+                  "CADT STUDENTS",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
